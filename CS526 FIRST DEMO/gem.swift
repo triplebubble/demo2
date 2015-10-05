785b56e5e652b0de2e56034f2f3e0eb6d89719b9
liv073 <luoyixiao@luoyixiaodeMacBook-Pro.local>
2015-10-05 10:34:51 -0700

    add Gem class

---
+++ CS526 FIRST DEMO/gem.swift
@@ -0,0 +1,18 @@
+//
+//  gem.swift
+//  CS526 FIRST DEMO
+//
+//  Created by luoyixiao on 15/10/5.
+//  Copyright © 2015年 User. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Gem: SKSpriteNode {
+    var colour: Int = 0
+    convenience init() {
+        self.init()
+    }
+}
+
