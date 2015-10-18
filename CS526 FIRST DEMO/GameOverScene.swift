eac6511947f4ab60498734f1f7d8d82f272d29e9
mingjiej <jackpot0704@gmail.com>
2015-10-17 21:08:33 -0700

    icon

--- CS526 FIRST DEMO/GameOverScene.swift
+++ CS526 FIRST DEMO/GameOverScene.swift
@@ -13,10 +13,24 @@ class GameOverScene: SKScene {
     let gameOverlabel: SKLabelNode
     let resultlabel: SKLabelNode
     let Mode: Int
+    let backgroundbottom1 = SKSpriteNode(imageNamed: "fightground_heishita.jpg")
+    let backgroundtop1 = SKSpriteNode(imageNamed: "fightground_heishita_EndPos.jpg")
+    let endboard = SKSpriteNode(imageNamed: "uiground_1.jpg")
+    var maxAspectRatio = CGFloat()
+    var playableMargin = CGFloat()
+    var maxAspectRatioWidth = CGFloat()
+    var playableRect = CGRect()
+
     init(size: CGSize, gameover: SKLabelNode, result: SKLabelNode, Number: Int){
         self.gameOverlabel = gameover
         self.resultlabel = result
         self.Mode = Number
+        maxAspectRatio = 16.0/9.0 // iPhone 5"
+        maxAspectRatioWidth = size.height / maxAspectRatio
+        playableMargin = (size.width - maxAspectRatioWidth) / 2.0
+        playableRect = CGRect(x: playableMargin, y: 0,
+            width: size.width - playableMargin/2,
+            height: size.height)
         super.init(size: size)
     }
     
@@ -27,6 +41,18 @@ class GameOverScene: SKScene {
         backgroundColor = UIColor.grayColor()
         self.addChild(gameOverlabel)
         self.addChild(resultlabel)
+        self.addChild(backgroundbottom1)
+        self.addChild(backgroundtop1)
+        self.addChild(endboard)
+        backgroundtop1.anchorPoint = CGPointZero
+        backgroundbottom1.anchorPoint = CGPointZero
+        backgroundbottom1.zPosition = 0
+        backgroundbottom1.position = CGPoint(x: playableMargin, y: 0)
+        backgroundtop1.zPosition = 0
+        backgroundtop1.position = CGPoint(x: playableMargin, y: backgroundbottom1.size.height)
+        endboard.position = CGPoint(x: size.width/2, y: size.height/2)
+        endboard.zPosition = 10
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
